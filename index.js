32ffc573eb4a23e5a69fbcb1a26e6bd1394658a3
Desablero <desablero@gmail.com>
2020-04-17 01:30:01 +0300

    Клавиатуры.
    Приветствие реагирует только на /start
    Установил forover: npm install forever -g, чтобы перезапускать бота по кнопке (не пригодилось)
    Сделал образцы обычной и инлайн клавиатур

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const TelegramBot = require ('node-telegram-bot-api'); // node js зависимость
+const TelegramBot = require('node-telegram-bot-api'); // node js зависимость
 const TOKEN = '894771621:AAHu1NFYyc5QKWH_5m5QQGco7VEVZUkXDUo'
-const bot = new TelegramBot (TOKEN, {
+const bot = new TelegramBot(TOKEN, {
     polling: {
         interval: 300, // милисекунд будет проходить между запросами клиента на сервер
         autoStart: true, // если юзер давал команду, но бот был выключен, тогда при включении бот обработает команду
@@ -9,20 +9,58 @@ const bot = new TelegramBot (TOKEN, {
         }
     }
 });
-const debug = require ("./helpers")/*  Polling - технология для связи клиента с сервером.
+const debug = require("./helpers")/*  Polling - технология для связи клиента с сервером.
     Мы как клиент запускаем на сервере тг. сервис который ожидает обновлений*/
 
-const search = {name:"search", text: "Поиск"}
-const setting = {name:"setting",text:"Настройки"}
-const profile = {name:"profile", text:`Профиль`}
+let search = {value: "/search", text: "Поиск"}
+let setting = {value: "setting", text: "Настройки"}
+let profile = {value: "/profile", text: `Профиль`}
+let popular = {value: "popular", text: `Популярные`}
 
 
-    bot.on ('message',(msg) => {const userId = msg.chat.id;
+bot.onText(/\/start/, (msg) => {
+    const userId = msg.chat.id;
     const greeting = `Привет, ` + msg.from.first_name + '! Меня зовут Графи 🐶 Граф! \nЯ могу найти для тебя любую дискографию из моего списка, просто зайди в меню "Поиск" и выбери желаемого исполнителя'
+    const mainMenu = {
+        reply_markup: {
+            keyboard: [
+                [profile.text, search.text],
+                [popular.text, setting.text]
+            ]
+        }
+    }
+    bot.sendMessage(userId, greeting)
+})
 
-        bot.sendMessage(userId, greeting)
 
-        })
+bot.onText(/Профиль/, (msg) => {
+    const userId = msg.chat.id;
+
+    const prof = {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: 'Премиум аккаунт',
+                        callback_data: '1'
+                    },
+                    {
+                        text: 'Реферальная система',
+                        callback_data: '2'
+                    }
+                ],
+                [
+                    {
+                        text: 'Избранные дискографии',
+                        callback_data: '3'
+                    }
+                ]
+            ]
+        }
+    }
+    bot.sendMessage(userId, 'Premium аккаунт: *отключён* \nДоступно дискографий: 3', prof)
+})
+
 
 /*
 // Служебное сообщение с инфой про юзера
@@ -41,7 +79,7 @@ bot.onText(/\/start (.+)/, (msg,[greeting]) => {
         bot.sendMessage(chatId, "🐶", {
             reply_markup: {
                 keyboard: [
-                    ["search.name"],
+                    [search, top],
                     [profile, setting]
                 ]
             }
@@ -111,11 +149,11 @@ bot.on("inline_query",query => {
     bot.answerInlineQuery(query.id, results, {
         cache_time: 0
     })
-})
-
-
-
-
 */
+
+
+
+
+
 console.log("All ok!")
 bot.on("polling_error", (err) => console.log(err));
