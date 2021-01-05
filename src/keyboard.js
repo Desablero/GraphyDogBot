d486a25e2c459c38c1538a99d57a553d5582921a
Desablero <desablero@gmail.com>
2021-01-05 15:07:48 +0300

    GRAPHY VER 2.0. Init clear project

--- src/keyboard.js
+++
@@ -1,9 +0,0 @@
-// Списки клавиатур для разных экранов.
-const kb = require('./keyboard-buttons')
-
-module.exports = {
-    home: [
-        [kb.home.films, kb.home.favorite]
-            [kb.home.cinemas]
-    ]
-}
